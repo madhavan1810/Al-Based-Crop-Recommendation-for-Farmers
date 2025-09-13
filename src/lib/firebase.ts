b7983c97837c467b1aab504847088347b3f5e279
shiva M <shivakumarM.reliance@gmail.com>
2025-09-13 08:56:50 +0000

    i literlly getting mad clear this it come like 100th thime "FirebaseErro

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,18 @@
-import { initializeApp, getApp, getApps, FirebaseApp } from 'firebase/app';
-import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
+import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
+import { getAuth, type Auth } from 'firebase/auth';
+import { getFirestore, type Firestore } from 'firebase/firestore';
 
-// This is public, client-side configuration.
 const firebaseConfig = {
-  apiKey: "AIzaSyC9lXNDFrDwnOgzv7tAXRYhOhHknkegOzU",
-  authDomain: "studio-684454327-e7713.firebaseapp.com",
-  projectId: "studio-684454327-e7713",
-  storageBucket: "studio-684454327-e7713.appspot.com",
-  messagingSenderId: "755577173726",
-  appId: "1:755577173726:web:f34edf42b5d53ad4c1744f"
+  apiKey: 'AIzaSyC9lXNDFrDwnOgzv7tAXRYhOhHknkegOzU',
+  authDomain: 'studio-684454327-e7713.firebaseapp.com',
+  projectId: 'studio-684454327-e7713',
+  storageBucket: 'studio-684454327-e7713.appspot.com',
+  messagingSenderId: '755577173726',
+  appId: '1:755577173726:web:f34edf42b5d53ad4c1744f',
 };
 
-// Initialize Firebase using a singleton pattern
+// Initialize Firebase for SSR
 const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
 
