3cabc02d93170fc26319c5edbebd9155a48331c6
shiva M <shivakumarM.reliance@gmail.com>
2025-09-15 01:37:29 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-// This file is intentionally left blank to disable the middleware.
-// The presence of this file can cause issues if not configured correctly.
-// We are leaving it empty to resolve routing errors after removing next-intl.
-export default function middleware() {}
-export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-};
