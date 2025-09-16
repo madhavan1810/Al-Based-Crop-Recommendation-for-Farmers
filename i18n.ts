92fa69b8b9502aec9d5a385e907681f2b9278036
shiva M <shivakumarM.reliance@gmail.com>
2025-09-16 00:55:17 +0000

    Try fixing this error: `Server Error: TypeError: Cannot read properties

--- i18n.ts
+++ i18n.ts
@@ -1,11 +1,12 @@
 import {getRequestConfig} from 'next-intl/server';
-import {allLocales as locales} from './src/lib/languages';
+import {allLocales} from './src/lib/languages';
  
 export const defaultLocale = 'en';
+export const locales = allLocales.map(l => l.code);
 
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.find(l => l.code === locale)) {
+  if (!locales.includes(locale)) {
     // Optionally, you could redirect to a default locale
     // or show a 404 page. For now, we'll just log an error.
     console.error(`Invalid locale: ${locale}`);
