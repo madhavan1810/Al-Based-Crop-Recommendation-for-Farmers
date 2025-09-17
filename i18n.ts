e1297925162c860100416aa29d1279e1981e2e44
shiva M <shivakumarM.reliance@gmail.com>
2025-09-17 04:55:57 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- i18n.ts
+++ i18n.ts
@@ -1,18 +1,12 @@
+import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
-import {locales, defaultLocale} from './src/lib/locales';
+import {locales} from './src/lib/locales';
  
 export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid.
-  if (!locales.includes(locale as any)) {
-    // If the locale is invalid, use the default locale.
-    return {
-      locale: defaultLocale,
-      messages: (await import(`./src/messages/${defaultLocale}.json`)).default
-    };
-  }
-
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) notFound();
+ 
   return {
-    locale,
     messages: (await import(`./src/messages/${locale}.json`)).default
   };
 });
