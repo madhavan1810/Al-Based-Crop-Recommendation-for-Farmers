2a69b5548d32303d90bc5d35960e856314d2c57c
shiva M <shivakumarM.reliance@gmail.com>
2025-09-19 01:47:09 +0000

    1 of 1 error Next.js (14.2.32) is outdated (learn more)
    Unhandled Runti

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,8 @@
 import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async ({locale}) => {
+  // This function is now only responsible for loading messages.
+  // The locale validation is handled in the middleware.
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   };
